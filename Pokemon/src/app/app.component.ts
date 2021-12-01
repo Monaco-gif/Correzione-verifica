b7e7c08d71f420c1769335a5e812a0d5abfdd15d
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-12-01 12:52:42 +0000

    Punto c da finire

---
+++ Pokemon/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps'; //punto c
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Pokemon';
+
+  
+
+}
