917e41d1acd8f64db4f97273c3b7db46a3f36342
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-12-13 17:52:32 +0000

    D

--- main.py
+++ main.py
@@ -10,5 +10,31 @@ CORS(app)
 def index():
     return send_file("coor.json")
 
+# punto d
+@app.route("/snorlax")
+def snorlax():
+    f = open('coor.json') 
+    data = json.load(f) 
+    return data['coor'][random.randrange(0,13)] 
+
+@app.route("/pikachu")
+def pikachu():
+    f = open('coor.json') 
+    data = json.load(f) 
+    return data['coor'][random.randrange(0,13)] 
+
+@app.route("/charmender")
+def charmender():
+    f = open('coor.json') 
+    data = json.load(f) 
+    return data['coor'][random.randrange(0,13)] 
+
+@app.route("/bulbasaur")
+def bulbasaur():
+    f = open('coor.json') 
+    data = json.load(f) 
+    return data['coor'][random.randrange(0,13)] 
+
+# fine punto d
 if __name__ == "__main__":
     app.run()
